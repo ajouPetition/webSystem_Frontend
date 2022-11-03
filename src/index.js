860085c86ec9df7fa372c3522f6dfe7c7ab68160
NSRBSG <nsrbsg@gmail.com>
2022-11-04 01:21:14 +0900

    ADD react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
 import './style/init.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
