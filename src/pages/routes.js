5798719061b294d7392ff93095ad9f47565f58b1
NSRBSG <nsrbsg@gmail.com>
2022-11-06 02:19:00 +0900

    Posts List 가져오기

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from './home/Home';
+import Petition from './home/Petition';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/petition',
+    element: <Petition />,
   },
 ]);
 
