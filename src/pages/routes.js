b9d14cb0c0344f4b0c2f49b278e78cfc2a7a2765
Sungpyo <pwsusc10@naver.com>
2022-11-12 07:48:30 +0900

    add login path

--- src/pages/routes.js
+++ src/pages/routes.js
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import Home from './Home';
+import Login from './Login/login';
 import PetitionList from './PetitionList';
 import PetitionWrite from './PetitionWrite';
 
@@ -8,6 +9,10 @@ const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path:'login',
+    element: <Login />
   },
   {
     path: '/petition',
