ca55a7af519b607c3bb72a35c6630363fa78bd7f
js1022003 <js1022003@ajou.ac.kr>
2022-11-26 20:41:57 +0900

    Card 컴포넌트 D-day 추가

--- src/components/PetitionCard.js
+++ src/components/PetitionCard.js
@@ -1,14 +1,27 @@
 import React from "react";
 import style from "../style/PetitionCard.module.css";
 
-const PetitionCard = ({ type, title }) => {
+const PetitionCard = ({ type, title, date, dueDate, dDay }) => {
   return (
     <li className={style.cardWrap}>
       <div className={style.contentDiv}>
         <div className={style.postType}>{type}</div>
         <div className={style.postTitle}>{title}</div>
       </div>
-      <div className={style.aggrementDiv}></div>
+      <div className={style.aggrementDiv}>
+        <div className={style.ratioDiv}>
+
+        </div>
+        <div className={style.dueDiv}>
+          <div className={style.dueDateDiv}>
+            <div className={style.dueDateHeader}>동의 기간</div>
+            <div className={style.dueDateFooter}>{`${date}~${dueDate}`}</div>
+          </div>
+          <div className={style.dDay}>
+            <p>{`D- ${dDay}`}</p>
+          </div>
+        </div>
+      </div>
     </li>
   );
 };
