f89aa53867c975b2f99f4a3b91cfea5f597a143d
Sungpyo <pwsusc10@naver.com>
2022-11-28 20:36:54 +0900

    로그인 수정

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import style from '../../style/Login.module.css';
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "../../style/Login.module.css";
 
 const Login = (props) => {
   const navigate = useNavigate();
@@ -10,54 +10,68 @@ const Login = (props) => {
 
   const onEmailHandler = (event) => {
     console.log(event.target.value);
-    setEmail(event);
-  }
+    setEmail(event.target.value);
+  };
 
   const onPasswordHandler = (event) => {
     console.log(event.target.value);
     setPassword(event.target.value);
-}
+  };
   const onCancleHandler = (event) => {
-    navigate('/');
-  }
+    navigate("/");
+  };
 
   const onLoginhandler = () => {
+    console.log(email);
+    console.log(password);
     axios
-      .post('//localhost:3000/api/users/login', {
-        email,
-        password
+      .post("//localhost:3000/api/users/auth", {
+        username: email,
+        password: password,
       })
-  }
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className={style.root}>
-
-    <section className={style.login}>
-      <div className={style.title}>로그인</div>
-      <div className={style.section}>
-        <input
-          type="text"
-          className={style.input}
-          placeholder='이메일'
-          onChange={onEmailHandler}
-          required
-        />
-        <input 
-          type="password"
-          className={style.input}
-          placeholder="비밀번호"
-          onChange={onPasswordHandler}
-          required 
-        />
-        <div className={style.btn}>
-          <button className={style.btn1} onClick={onLoginhandler}>로그인</button>
-          <button className={style.btn1} onClick={onCancleHandler}>취소</button>
+      <section className={style.login}>
+        <div className={style.title}>로그인</div>
+        <div className={style.section}>
+          <input
+            type="text"
+            className={style.input}
+            placeholder="이메일"
+            onChange={onEmailHandler}
+            required
+          />
+          <input
+            type="password"
+            className={style.input}
+            placeholder="비밀번호"
+            onChange={onPasswordHandler}
+            required
+          />
+          <div className={style.btn}>
+            <button className={style.btn1} onClick={onLoginhandler}>
+              로그인
+            </button>
+            <button className={style.btn1} onClick={onCancleHandler}>
+              취소
+            </button>
+          </div>
         </div>
-      </div>
-      <p>회원가입은 <a className={style.href} href={'/login/signup'}>여기</a>에서 할 수 있습니다.</p>
-    </section>
+        <p>
+          회원가입은{" "}
+          <a className={style.href} href={"/login/signup"}>
+            여기
+          </a>
+          에서 할 수 있습니다.
+        </p>
+      </section>
     </div>
-    
   );
-}
+};
 
 export default Login;
