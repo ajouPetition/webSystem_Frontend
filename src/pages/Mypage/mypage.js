8e807cc358f2dc2d6a0228aa3d9a0009ed94ec7d
Sungpyo <pwsusc10@naver.com>
2022-12-09 23:57:52 +0900

    add my page pagination

--- src/pages/Mypage/mypage.js
+++ src/pages/Mypage/mypage.js
@@ -1,38 +1,45 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import style from "../../style/Mypage.module.css";
 import cookies from "react-cookies";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import PetitionCard from "../../components/PetitionCard";
-import { toHaveDisplayValue } from "@testing-library/jest-dom/dist/matchers";
 const Mypage = (props) => {
   const navigate = useNavigate();
   const [myWrite, setMyWrite] = useState([]);
   const [myAgree, setMyAgree] = useState([]);
+  const [pagenum, setPagenum] = useState();
+  const [page, setPage] = useState(1);
 
   const getWrite = async () => {
     await axios
       .get(`http://localhost:8080/api/users/posts`, {
         params: {
           username: `${cookies.load("userid")}`,
-          startAt: 0,
-          limit: 3,
+          startAt: 3 * (page - 1),
+          limit: 3 * page,
         },
       })
       .then((data) => {
-        // data.data.map((data) => {
-        //   const today = new Date();
-        //   const date = new Date(data.date).getDay();
-        //   const dueDate = new Date(
-        //     new Date(data.date).setDate(new Date(data.date).getDate() + 30)
-        //   );
-        //   const dDay = Math.ceil(
-        //     (dueDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
-        //   );
-        //   data.dueDate = dueDate;
-        //   data.dDay = dDay;
-        // });
         setMyWrite(data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    await axios
+      .get(`http://localhost:8080/api/users/posts`, {
+        params: {
+          username: `${cookies.load("userid")}`,
+          startAt: 0,
+          limit: 100,
+        },
+      })
+      .then((data) => {
+        if (data.data.length % 3 == 0) {
+          setPagenum(parseInt(data.data.length / 3));
+        } else {
+          setPagenum(parseInt(data.data.length / 3) + 1);
+        }
       })
       .catch((err) => {
         console.log(err);
@@ -41,7 +48,6 @@ const Mypage = (props) => {
   const writeHandler = (event) => {
     setMyAgree([]);
     getWrite();
-    console.log(myWrite);
   };
 
   const getAgree = async () => {
@@ -59,12 +65,29 @@ const Mypage = (props) => {
       .catch((err) => {
         console.log(err);
       });
+    await axios
+      .get(`http://localhost:8080/api/users/agree`, {
+        params: {
+          username: `${cookies.load("userid")}`,
+          startAt: 0,
+          limit: 3,
+        },
+      })
+      .then((data) => {
+        if (data.data.length % 3 == 0) {
+          setPagenum(parseInt(data.data.length / 3));
+        } else {
+          setPagenum(parseInt(data.data.length / 3) + 1);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const agreeHandler = (event) => {
     setMyWrite([]);
     getAgree();
-    console.log(myAgree);
   };
 
   const logoutHandler = (event) => {
@@ -86,6 +109,18 @@ const Mypage = (props) => {
       })
       .catch((err) => console.log(err));
   };
+  const pageHandler = (event) => {
+    console.log(event.target.innerText);
+    setPage(event.target.innerText);
+  };
+
+  useEffect(() => {
+    if (myAgree.length === 0) {
+      getWrite();
+    } else {
+      getAgree();
+    }
+  }, [page]);
   return (
     <div className={style.section}>
       <div className={style.optionbar}>
@@ -116,50 +151,65 @@ const Mypage = (props) => {
         </div>
       </div>
       <div className={style.info}>
-        {myWrite.map((data) => {
-          const today = new Date();
-          const date = new Date(data.date);
-          const dueDate = new Date(
-            new Date(data.date).setDate(new Date(data.date).getDate() + 30)
-          );
-          const dDay = Math.ceil(
-            (dueDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
-          );
-          return (
-            <PetitionCard
-              key={data.postID}
-              type={data.type}
-              title={data.title}
-              date={date.toLocaleDateString()}
-              dueDate={dueDate.toLocaleDateString()}
-              dDay={dDay}
-              postID={data.postID}
-              cnt={data.cnt}
-            />
-          );
-        })}
-        {myAgree.map((data) => {
-          const today = new Date();
-          const date = new Date(data.date);
-          const dueDate = new Date(
-            new Date(data.date).setDate(new Date(data.date).getDate() + 30)
-          );
-          const dDay = Math.ceil(
-            (dueDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
-          );
-          return (
-            <PetitionCard
-              key={data.postID}
-              type={data.type}
-              title={data.title}
-              date={date.toLocaleDateString()}
-              dueDate={dueDate.toLocaleDateString()}
-              dDay={dDay}
-              postID={data.postID}
-              cnt={data.cnt}
-            />
-          );
-        })}
+        <ul className={style.data}>
+          {myWrite.map((data) => {
+            const today = new Date();
+            const date = new Date(data.date);
+            const dueDate = new Date(
+              new Date(data.date).setDate(new Date(data.date).getDate() + 30)
+            );
+            const dDay = Math.ceil(
+              (dueDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
+            );
+            return (
+              <PetitionCard
+                key={data.postID}
+                type={data.type}
+                title={data.title}
+                date={date.toLocaleDateString()}
+                dueDate={dueDate.toLocaleDateString()}
+                dDay={dDay}
+                postID={data.postID}
+                cnt={data.cnt}
+              />
+            );
+          })}
+          {myAgree.map((data) => {
+            const today = new Date();
+            const date = new Date(data.date);
+            const dueDate = new Date(
+              new Date(data.date).setDate(new Date(data.date).getDate() + 30)
+            );
+            const dDay = Math.ceil(
+              (dueDate.getTime() - today.getTime()) / (1000 * 60 * 60 * 24)
+            );
+            return (
+              <PetitionCard
+                key={data.postID}
+                type={data.type}
+                title={data.title}
+                date={date.toLocaleDateString()}
+                dueDate={dueDate.toLocaleDateString()}
+                dDay={dDay}
+                postID={data.postID}
+                cnt={data.cnt}
+              />
+            );
+          })}
+        </ul>
+        <div className={style.pagenumber}>
+          {Array(pagenum)
+            .fill()
+            .map((_, num) => (
+              <button
+                key={num}
+                className={style.pagebutton}
+                onClick={pageHandler}
+              >
+                {num + 1}
+              </button>
+            ))}
+        </div>
       </div>
     </div>
   );
