0373d346157360749de46262217f47d518f9d3ac
NSRBSG <nsrbsg@gmail.com>
2022-12-10 18:04:36 +0900

    로그인 안되어있을시 로그인페이지로 리다이렉션

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,16 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import NavigationBar from "../components/NavigationBar";
-import "../style/init.css";
-import Home from "./Home";
-import Login from "./Login/login";
-import Mypage from "./Mypage/mypage";
-import PetitionDetail from "./PetitionDetail";
-import PetitionList from "./PetitionList";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NavigationBar from '../components/NavigationBar';
+import '../style/init.css';
+import Home from './Home';
+import Login from './Login/login';
+import Mypage from './Mypage/mypage';
+import PetitionDetail from './PetitionDetail';
+import PetitionList from './PetitionList';
 import PetitionCompletedList from './PetitionCompletedList';
-import PetitionWrite from "./PetitionWrite";
-import Signup from "./Signup/signup";
+import PetitionWrite from './PetitionWrite';
+import Signup from './Signup/signup';
+import cookies from 'react-cookies';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    setUser(cookies.load('userid'));
+  }, []);
+
   return (
     <BrowserRouter>
       <NavigationBar />
@@ -19,8 +27,16 @@ function App() {
         <Route path="/login" exact element={<Login />}></Route>
         <Route path="/login/signup" exact element={<Signup />}></Route>
         <Route path="/petition" exact element={<PetitionList />}></Route>
-        <Route path="/petitionCompleted" exact element={<PetitionCompletedList />}></Route>
-        <Route path="/petition/write" exact element={<PetitionWrite />}></Route>
+        <Route
+          path="/petitionCompleted"
+          exact
+          element={<PetitionCompletedList />}
+        ></Route>
+        <Route
+          path="/petition/write"
+          exact
+          element={user ? <PetitionWrite /> : <Login />}
+        ></Route>
         <Route path="/mypage" exact element={<Mypage />}></Route>
         <Route
           path="/petition/detail/:id"
