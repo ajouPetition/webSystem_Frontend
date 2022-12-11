a1c1785fe647f1a9e4128d221eec208712192266
Sungpyo <pwsusc10@naver.com>
2022-12-11 12:16:54 +0900

    회원탈퇴 수정

--- src/pages/Login/login.js
+++ src/pages/Login/login.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import style from '../../style/Login.module.css';
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import style from "../../style/Login.module.css";
 
 const Login = ({ setCookie }) => {
   const navigate = useNavigate();
@@ -20,20 +20,27 @@ const Login = ({ setCookie }) => {
 
   const onLoginhandler = () => {
     axios
-      .post('http://localhost:8080/api/users/login', {
+      .post("http://localhost:8080/api/users/login", {
         username: username,
         password: password,
       })
       .then((data) => {
         console.log(data);
 
-        // cookie 저장
-
-        setCookie('token', data.data.token);
-        // 로그인 성공 후 홈 화면으로
-        navigate('/');
+        if (data.data.status === "success") {
+          alert("로그인 성공");
+          // cookie 저장
+          setCookie("token", data.data.token);
+          // 로그인 성공 후 홈 화면으로
+          navigate("/");
+        } else {
+          alert("로그인 실패");
+        }
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        alert("로그인 실패");
+      });
   };
   return (
     <div className={style.root}>
@@ -61,8 +68,8 @@ const Login = ({ setCookie }) => {
           </div>
         </div>
         <div>
-          회원가입은{' '}
-          <a className={style.href} href={'/login/signup'}>
+          회원가입은{" "}
+          <a className={style.href} href={"/login/signup"}>
             여기
           </a>
           에서 할 수 있습니다.
